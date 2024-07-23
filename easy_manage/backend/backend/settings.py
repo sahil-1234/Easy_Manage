127604885abc4463f7483af9223d175019ad3dcc
SAHIL SRIVASTAV <96813045+sahil-1234@users.noreply.github.com>
2024-07-23 22:30:29 +0530

    Update settings.py

--- easy_manage/backend/backend/settings.py
+++ easy_manage/backend/backend/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'YOUR SECRET KEY'

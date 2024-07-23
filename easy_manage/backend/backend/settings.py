818fc3314ceb479083ebdf79b62ac3546fe0ece7
SAHIL SRIVASTAV <96813045+sahil-1234@users.noreply.github.com>
2024-07-23 22:30:41 +0530

    Update settings.py

--- easy_manage/backend/backend/settings.py
+++ easy_manage/backend/backend/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+
 
 
 # SECURITY WARNING: keep the secret key used in production secret!

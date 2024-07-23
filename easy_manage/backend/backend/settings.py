ffba83b0374327fd8070e94bf4875b9fac280aeb
SAHIL SRIVASTAV <96813045+sahil-1234@users.noreply.github.com>
2024-07-23 22:30:53 +0530

    Update settings.py

--- easy_manage/backend/backend/settings.py
+++ easy_manage/backend/backend/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!

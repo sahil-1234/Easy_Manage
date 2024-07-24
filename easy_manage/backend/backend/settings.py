6820a956c073a228641b6c1a474c9c740bdb1d0d
SAHIL SRIVASTAV <96813045+sahil-1234@users.noreply.github.com>
2024-07-24 22:55:22 +0530

    Update settings.py

--- easy_manage/backend/backend/settings.py
+++ easy_manage/backend/backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'YOUR SECRET KEY'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []

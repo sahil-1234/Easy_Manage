d79e4e67c293c24a7f772cf9abc0ddd2a6c061cf
SAHIL SRIVASTAV <96813045+sahil-1234@users.noreply.github.com>
2024-07-28 23:07:49 +0530

    Update urls.py

--- easy_manage/backend/backend/urls.py
+++ easy_manage/backend/backend/urls.py
@@ -1,13 +1,4 @@
-"""
-URL configuration for backend project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from todo import views

ec57dbbc5f09481866124b2382e39d65f00c3f17
SAHIL SRIVASTAV <96813045+sahil-1234@users.noreply.github.com>
2024-07-29 22:46:24 +0530

    Update wsgi.py

--- easy_manage/backend/backend/wsgi.py
+++ easy_manage/backend/backend/wsgi.py
@@ -1,7 +1,6 @@
 """
 
 
-It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/

acc4f72d28dc792b38f5620b7a47aeedb6e73042
SAHIL SRIVASTAV <96813045+sahil-1234@users.noreply.github.com>
2024-08-02 22:22:37 +0530

    Update settings.py

--- easy_manage/backend/backend/settings.py
+++ easy_manage/backend/backend/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'todo',
     'corsheaders',            # add this
-    'rest_framework',         # add this
+    'rest_framework',       
 ]
 
 MIDDLEWARE = [
